365ad93849b86124903b85572b1055a22181a86b
ayaanmustafa <69399872+ayaanmustafa@users.noreply.github.com>
2026-03-08 17:09:36 +0530

    Update serverHosted.py

--- serverHosted.py
+++ serverHosted.py
@@ -4,13 +4,16 @@ import os
 
 clients = {}
 
+async def process_request(path, request_headers):
+    # respond to normal HTTP requests
+    return (200, [], b"Chat server running\n")
+
 async def handler(ws):
     username = await ws.recv()
     clients[ws] = username
 
     try:
         async for msg in ws:
-
             if msg.lower() == "exit":
                 await ws.close()
                 break
@@ -33,8 +36,13 @@ async def handler(ws):
 PORT = int(os.environ.get("PORT", 8765))
 
 async def main():
-    async with websockets.serve(handler, "0.0.0.0", PORT):
-        print(f"Server running on port {PORT}")
+    async with websockets.serve(
+        handler,
+        "0.0.0.0",
+        PORT,
+        process_request=process_request
+    ):
+        print("Server running")
         await asyncio.Future()
 
 asyncio.run(main())
